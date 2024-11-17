43d6a9c5ef84a2e7b2c35796bee6a0368ff1e905
NayanCod <kumarnayab6@gmail.com>
2024-11-17 13:10:56 +0530

    Initalized the project

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+    Heloworld
+    </div>
+  );
+}
+
+export default App;
